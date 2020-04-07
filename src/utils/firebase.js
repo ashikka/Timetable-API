2935bdd2304594e89cae667a1fcd91dcebccfdd8
ashikka <ashikagupta28@gmail.com>
2020-04-07 15:49:44 +0530

    Adding database

---
+++ src/utils/firebase.js
@@ -0,0 +1,14 @@
+/*jshint esversion: 6 */
+
+const admin = require('firebase-admin');
+const dotenv = require("dotenv");
+dotenv.config();
+
+var serviceAccount = require(process.env.CREDS)
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: process.env.DATABASE_URL,
+});
+
+const database = admin.firestore()
+module.exports = {admin,database}
