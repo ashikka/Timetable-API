47f9d91760c5a5c1c259cc7e3c56299659fcd8d7
ashikka <ashikagupta28@gmail.com>
2020-04-18 18:56:20 +0530

    Adding CRUD options

--- routes/posts.js
+++ routes/posts.js
@@ -4,10 +4,9 @@ const express = require("express");
 const router = express.Router();
 const Timetable = require("../models/Timetable");
 
-router.get("/", function(req,res){
-  res.send("Hey");
-});
+const someArray= [];
 
+//Routes targeting all users
 router.post("/", async function(req,res){
   const timetable = new Timetable({
     status : req.body.status,
@@ -26,5 +25,66 @@ router.post("/", async function(req,res){
     res.json({message : err});
   }
 });
+
+router.get("/:personName", function(req,res){
+  Timetable.findOne({name : req.params.personName}, function(err, foundName){
+    if(foundName){
+      // foundName.monday.forEach(element => {
+      //   Object.keys(element).forEach((el) =>{
+      //     if(element[el].free === true)
+      //     {
+      //       someArray.push(el);
+      //     }
+      //   });
+      //   res.send(someArray);
+      // });
+      res.send(foundName);
+    }else{
+      res.send(err);
+    }
+  });
+});
+ 
+//Routes targeting specific user
+router.put("/:personName",function(req,res){
+  Timetable.update(
+      {name : req.params.personName},
+      {status: req.body.status , name : req.body.name , regNo : req.body.regNo, monday: req.body.monday , tuesday: req.body.tuesday , wednesday: req.body.wednesday, thursday: req.body.thursday, friday: req.body.friday},
+      {overwrite : true},
+      function(err){
+        if(!err){
+          res.send("Successfully updated using put request");
+        }
+      }
+  );
+});
+
+router.patch("/:personName", function(req,res){
+  Timetable.update(
+    {name : req.params.personName},
+    {$set : req.body},
+    function(err){
+      if(!err){
+        res.send("Updated using patch request");
+      }else{
+        res.send(err);
+      }
+    });
+});
+
+router.delete("/:personName",function(req,res){
+  Timetable.deleteOne(
+    {name : req.params.personName},
+  function(err){
+    if(err){
+      res.send(err);
+    }
+    else{
+      res.send("deleted");
+    }
+  }
+  );
+});
+
 
 module.exports = router;
